10967d6502c3c9956b405786d4b25cd18178e95f
AcsaiFZ <acsai.fz@gmail.com>
2022-04-15 21:01:17 +0200

    Initialize coffees and coffeeMachine

--- src/hu/progmatic/Main.java
+++ src/hu/progmatic/Main.java
@@ -1,8 +1,19 @@
 package hu.progmatic;
 
+import hu.models.Coffee;
+import hu.models.CoffeeMachine;
+
 public class Main {
 
     public static void main(String[] args) {
+        Coffee[] coffees = {
+                new Coffee("Rövid kávé",50,6,6,70),
+                new Coffee("Rövid kávé tejjel", 60, 5, 6, 6, 70),
+                new Coffee("Hosszú kávé", 100, 12,9,100),
+                new Coffee("Hosszú kávé tejjel", 120, 20, 12,9,100),
+                new Coffee("Cappuccino", 150,30,6,9,120)
+        };
 
+        CoffeeMachine coffeeMachine = new CoffeeMachine(coffees);
     }
 }
